9314321c6301069eea1ea6f6a5aabdcc8e0e6864
Melis Gökalp <melisgokalp@gmail.com>
2019-05-10 13:57:05 -0400

    clean up

--- our_model.py
+++ our_model.py
@@ -2,26 +2,21 @@ import numpy as np
 from skimage.transform import rescale
 import tensorflow as tf
 from PIL import Image
-import numpy as np
 from pylab import imshow, show, get_cmap
 from keras import backend as K
 from keras.preprocessing import image
-from keras.applications.vgg16 import VGG16
-from keras.applications.vgg19 import VGG19      
-from skimage.exposure import adjust_gamma
-from keras.applications.vgg19 import preprocess_input
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+from keras.applications.vgg19 import VGG19, preprocess_input
 from keras.models import Model
 from numpy import random
 import time
 from scipy.optimize import fmin_l_bfgs_b
 from scipy import ndimage
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import os
 import errno
-from datetime import datetime
 
-# activate gpu/cpu
+# activate gpu/cpu when runnning on ssh
  #source /course/cs1430/tf_gpu/bin/activate
  # to clear up memory
  #kill $(jobs -p)
@@ -64,25 +59,22 @@ POS_CONTENT, POS_STYLE, POS_COMBINED = (0, 1, 2)
 content_layer = ['block4_conv2'] 
 style_layer = ['block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1', 'block5_conv1']
 num_iterations=100 
-results = []
+content_weight =  1.0
+style_weight= 0.5
+
 #parameters for style and content, later used for saving
 style = 'kandinsky'
 content = 'galata'
 style_path = 'images/' + style + '.jpg'
 content_path = 'images/' + content + '.jpg'
-i_style = load_data(style_path) 
-i_content = load_data(content_path)
-img_h = i_content.shape[1]
-img_w = i_content.shape[2]
+i_style, i_content = load_data(style_path), load_data(content_path) 
+img_h, img_w = i_content.shape[1], i_content.shape[2]
 i_combined = K.placeholder((1, img_h, img_w, 3))
  
 combined_tensor = K.concatenate([i_content, i_style, i_combined], axis=0)
 
 # Initialize VGG-19 Model using Keras 
 model = VGG19(input_tensor=combined_tensor, weights='imagenet', include_top=False)
- 
-content_weight =  1.0
-style_weight= 0.5
 
 def calc_loss(model, combined_image):
         layer_outputs = dict([layer.name, layer.output] for layer in model.layers)
@@ -110,7 +102,6 @@ gradients = K.gradients(total_loss, i_combined)[0]
 kfunc = K.function([i_combined], [total_loss, gradients])
 
 class Evaluator(object):
-    
     def __init__(self):
         self.loss_value = None
         self.grads_values = None
@@ -131,17 +122,15 @@ class Evaluator(object):
         self.loss_value = None
         self.grad_values = None
         return grad_values
+
+#initialize the evaluator
 evaluator = Evaluator()
-
-
-min_loss = float('inf')
-best_i_combined = None
+ 
 x = load_data(content_path)
-
 # experiment with white noise image
 # x = np.random.uniform(0, 255, (1, img_h, img_w, 3)) - 128.
-
 x = x.flatten()
+ 
 for i in range(num_iterations):
         mf = 20
         start_time = time.time()
@@ -149,20 +138,15 @@ for i in range(num_iterations):
                                         x,
                                         fprime=evaluator.grads,
                                         maxfun= mf)
-        print('Loxx:', min_val)
-
-        #### OUR STUPID MISTAKE HERE #####
-        # x = deprocess_image(x, img_h, img_w)
-        ##################################
+        print('Loss:', min_val)
+ 
         img = deprocess_image(x, img_h, img_w)
-        
         #used a median filter to reduce noise
         img = ndimage.median_filter(img, 3) 
 
-     
-        file = 'images/at%d,(%f,%f,%d).png' % (i,content_weight,style_weight, mf)
         end_time = time.time()
         print('This iteration %d completed in %ds' % (i, end_time - start_time))
+        
         filename = "images/%s,%s(%f,%f,%d)/at%d.png" % (content,style,content_weight,style_weight, mf, i)
         #create a new folder for each run style-content pair
         os.makedirs(os.path.dirname(filename), exist_ok=True)
